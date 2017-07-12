3ffc8c88bcafc8db498fb7b088489f1c9e5f64a1
Jorge Encalada Fuentes <FotTgames@gmail.com>
2017-07-12 02:36:28 -0400

    cambios menores

--- application/controllers/Patentes.php
+++ application/controllers/Patentes.php
@@ -14,11 +14,38 @@
 			$data['titulo'] = "Todas las patentes";
 			$data['currentPage'] = "Patentes";
 			$datosPatentes['patentes'] = $this->PatentesModel->getAllPatentes();
+			//$this->load->library('pagination');
+			//$config = array();
+			//$config['total_rows'] = $this->PatentesModel->count_actor();
+			//$config['per_page'] = 10;
+			//$config['base_url'] = 'http://localhost/taller-integrado/';
+			
+			//$data['links'] = $this->pagination->create_links();
 			$this->load->library('pagination');
+
+			$config['base_url'] = base_url().'Patentes/index';
 			$config['total_rows'] = $this->PatentesModel->count_actor();
-			$config['per_page'] = 10;
-			$this->pagination->initialize($config);
+			$config['per_page'] = 8;
 			$data['actor'] = $this->PatentesModel->fetch_actor($config['per_page'], $this->uri->segment(3));
+
+			$config['full_tag_open'] = "<ul class='pagination'>";
+			$config['full_tag_close'] = "</ul>";
+			$config['num_tag_open'] = "<li>";
+			$config['num_tag_close'] = "</li>";
+			$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+			$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+			$config['next_tag_open'] = "<li>";
+			$config['next_tagl_close'] = "</li>";
+			$config['prev_tag_open'] = "<li>";
+			$config['prev_tagl_close'] = "</li>";
+			$config['first_tag_open'] = "<li>";
+			$config['first_tagl_close'] = "</li>";
+			$config['last_tag_open'] = "<li>";
+			$config['last_tagl_close'] = "</li>";
+
+			$this->pagination->initialize($config);
+
+			
 
 			$this->load->view('plantilla/header', $data);
 			$this->load->view('plantilla/navbar');
--- application/views/plantilla/Footer.php
+++ application/views/plantilla/Footer.php
@@ -18,39 +18,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
-    <!--<script src="<?php echo base_url() ?>assets/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url() ?>assets/datatables/dataTables.bootstrap.min.js"></script>
-    <script>
-		$(function () {
-			$('#patentes').DataTable({
-				"paging": true,
-				"lengthChange": true,
-				"searching": true,
-				"ordering": true,
-				"info": true,
-				"autoWidth": true,
-				"pagingType": "full_numbers",
-				"language": {
-					"lengthMenu": "Mostrando _MENU_ registros por página",
-					"zeroRecords": "No se encontraron registros",
-					"info": "Mostrando página _PAGE_ de _PAGES_",
-					"infoEmpty": "No hay registros disponibles",
-					"infoFiltered": "(Filtrado de _MAX_ registros totales)",
-					"search": "Buscar: ",
-					"decimal": ",",
-					"thousands": ".",
-					"loadingRecords": "Cargando...",
-					"processing":     "Procsando...",
-					"emptyTable":     "No hay informacion disponible",
-					"paginate": {
-				        "first":      "Primero",
-				        "last":       "Último",
-				        "next":       "Siguiente",
-				        "previous":   "Anterior"
-				    }
-				}
-			});
-		});
-  </script>-->
 </body>
 </html>
